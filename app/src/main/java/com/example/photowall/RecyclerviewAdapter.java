c73ad64352b88b2f97ee71433b0eb59207e02d38
“your_username” <67698619@qq.com>
2018-05-07 17:45:11 +0800

    by myself

---
+++ app/src/main/java/com/example/photowall/RecyclerviewAdapter.java
@@ -0,0 +1,42 @@
+package com.example.photowall;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import com.example.photowall.R;
+/**
+ * Created by 67698 on 2018/5/7.
+ */
+
+public class RecyclerviewAdapter extends RecyclerView.Adapter <RecyclerviewAdapter.ViewHolder>{
+   private Context mContext;
+    @Override
+    public RecyclerviewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if(mContext==null)mContext=parent.getContext();
+        View view= LayoutInflater.from(mContext).inflate(R.layout.)
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerviewAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        ImageView imageView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            cardView=(CardView)itemView;
+            imageView=(ImageView)itemView.findViewById(R.id.);
+        }
+    }
+}
