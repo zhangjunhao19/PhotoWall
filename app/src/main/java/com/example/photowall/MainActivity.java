e68af9f1909db9160683f6c99e583f062cf73df4
“your_username” <67698619@qq.com>
2018-05-08 09:16:31 +0800

    by myself

--- app/src/main/java/com/example/photowall/MainActivity.java
+++ app/src/main/java/com/example/photowall/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.photowall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] Photo={"https://ws1.sinaimg.cn/large/610dc034ly1fp9qm6nv50j20u00miacg.jpg",
+            "https://ws1.sinaimg.cn/large/610dc034ly1foowtrkpvkj20sg0izdkx.jpg",
+            "http://7xi8d6.com1.z0.glb.clouddn.com/20180208080314_FhzuAJ_Screenshot.jpeg",
+            "http://7xi8d6.com1.z0.glb.clouddn.com/20180122090204_A4hNiG_Screenshot.jpeg",
+            "http://7xi8d6.com1.z0.glb.clouddn.com/20180115085556_8AeReR_taeyeon_ss_15_1_2018_7_58_51_833.jpeg",
+            "http://7xi8d6.com1.z0.glb.clouddn.com/20180109085038_4A7atU_rakukoo_9_1_2018_8_50_25_276.jpeg"
 
+    };
+    private RecyclerviewAdapter recyclerviewAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        RecyclerView recyclerView=findViewById(R.id.recyclerview);
+        GridLayoutManager gridLayoutManager=new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerviewAdapter=new RecyclerviewAdapter(this,Photo);
+        recyclerView.setAdapter(recyclerviewAdapter);
     }
 }
--- app/src/main/java/com/example/photowall/RecyclerviewAdapter.java
+++ app/src/main/java/com/example/photowall/RecyclerviewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.photowall;
 
 import android.content.Context;
+import android.provider.ContactsContract;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -14,6 +15,12 @@ import com.example.photowall.R;
 
 public class RecyclerviewAdapter extends RecyclerView.Adapter <RecyclerviewAdapter.ViewHolder>{
    private Context mContext;
+   private String[] Photos;
+   private ImageLoader imageLoader;
+   public RecyclerviewAdapter(Context context,String[] Photos ){
+       this.Photos=Photos;
+        imageLoader=ImageLoader.build(context);
+   }
     @Override
     public RecyclerviewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         if(mContext==null)mContext=parent.getContext();
@@ -23,12 +30,12 @@ public class RecyclerviewAdapter extends RecyclerView.Adapter <RecyclerviewAdapt
 
     @Override
     public void onBindViewHolder(RecyclerviewAdapter.ViewHolder holder, int position) {
-
+        imageLoader.bindBitmap(Photos[position],holder.imageView,holder.imageView.getWidth(),holder.itemView.getHeight());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return Photos.length;
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
